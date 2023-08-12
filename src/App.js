d21fd6ca62d803d2f9fda937aebd891bcf9e6500
Alex Murillo <amurillo_g@hotmail.com>
2023-08-11 19:41:38 -0600

    Start header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Header from './Header';
 function App() {
   return (
     <div className="app">
       <h1>Que Rollo Todos</h1>
+      <Header />
       {/*HEADER*/}
       {/*HOME*/}
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Header.css';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <img className='header__logo' src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+      <div className="header__search">
+        <input type="text" className="header__search" />
+        {/* LOGO*/}
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__OptionLineOne">Hello Guest</span>
+          <span className="header_OptionLineTwo">Sign In</span> 
+        </div>
+        <div className="header__option">
+          <span className="header__OptionLineOne">Returns</span>
+          <span className="header_OptionLineTwo">& orders</span> 
+        </div>
+        <div className="header__option">
+          <span className="header__OptionLineOne">Your</span>
+          <span className="header_OptionLineTwo">Prime</span> 
+        </div>
+      </div>
+    </div>
   )
 }
 
