4727c6a965e794d61c0c929c68183ab2b2c68215
Alex Murillo <amurillo_g@hotmail.com>
2023-08-19 20:04:29 -0600

    casi terminar home

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,58 @@
 import React from 'react'
-import './home.css'
+import './Home.css'
+import Product from './Product'
 
 function Home() {
   return (
     <div className="home">
         <div className="home__container">
-            <img src="https://images.pexels.com/photos/697244/pexels-photo-697244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <img src="https://images.pexels.com/photos/697244/pexels-photo-697244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""  className='home__image'/>
+
+            <div className="home__row">
+              <Product
+                id="54554564" 
+                title= "vestido sexy"
+                price={102.99}
+                image="https://images.pexels.com/photos/4132651/pexels-photo-4132651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                rating={3}
+              />
+              <Product 
+                id="54554564" 
+                title= "vestido sexy color verde con tirantes marca fashionbox"
+                price={102.99}
+                image="https://images.pexels.com/photos/4132651/pexels-photo-4132651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </div>
+
+            <div className="home__row">
+              <Product 
+                id="54554564" 
+                title= "vestido sexy"
+                price={102.99}
+                image="https://images.pexels.com/photos/4132651/pexels-photo-4132651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+              <Product 
+                id="54554564" 
+                title= "vestido sexy"
+                price={102.99}
+                image="https://images.pexels.com/photos/4132651/pexels-photo-4132651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+              <Product 
+                id="54554564" 
+                title= "vestido sexy"
+                price={102.99}
+                image="https://images.pexels.com/photos/4132651/pexels-photo-4132651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </div>
+
+            <div className="home__row">
+              <Product 
+                id="54554564" 
+                title= "vestido sexy"
+                price={102.99}
+                image="https://images.pexels.com/photos/4132651/pexels-photo-4132651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </div>
         </div>
     </div>
   )
